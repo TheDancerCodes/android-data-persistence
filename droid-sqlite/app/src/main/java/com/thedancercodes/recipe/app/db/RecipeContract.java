531a208c391572ff9ebc9c3a11ff1d731cfe6185
Roger Taracha <rojtaracha@gmail.com>
2018-06-14 12:11:31 +0300

    [FEATURE] Define a database tables schema:
    [+] Create a RecipeEntry class in the RecipeContract class & set up column names for all the key fields in the recipe table.

--- droid-sqlite/app/src/main/java/com/thedancercodes/recipe/app/db/RecipeContract.java
+++ droid-sqlite/app/src/main/java/com/thedancercodes/recipe/app/db/RecipeContract.java
@@ -2,14 +2,46 @@ package com.thedancercodes.recipe.app.db;
 
 import android.provider.BaseColumns;
 
-final class RecipeContract
-{
-    private RecipeContract() {}
+/**
+ * Contract Class:
+ * A container class that holds the constants we need to define the table names & columns.
+  */
+final class RecipeContract {
 
-    public static class RecipeStepEntry implements BaseColumns
-    {
+    // Constructor
+    private RecipeContract() {
+    }
+
+
+    // Reference fields to the Recipe class
+    private long id;
+
+    private String name;
+
+    private String description;
+
+    private int imageResourceId;
+
+    // RecipeEntry class implements BaseColumns Interface
+    public static class RecipeEntry implements BaseColumns {
+
+        // Set up column names for all the key fields in the recipe table.
+        // NB: We don't add one for the ID because we get it for free in the BaseColumns class.
+        public static final String TABLE_NAME = "recipe";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_IMAGE_RESOURCE_ID = "image_resource_id";
+
+    }
+
+
+    // RecipeStepEntry class implements BaseColumns Interface
+    public static class RecipeStepEntry implements BaseColumns {
+
+        // The column names to include here are based on fields included in the model class.
+        // In the RecipeStep class we will see these fields as members.
         public static final String TABLE_NAME = "recipe_step";
-        public static final String COLUMN_RECIPE_ID = "recipe_id";
+        public static final String COLUMN_RECIPE_ID = "recipe_id"; // Foreign key
         public static final String COLUMN_STEP_NUMBER = "step_number";
         public static final String COLUMN_INSTRUCTION = "instruction";
     }
