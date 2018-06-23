a5277305781d7412d30ab32bcbe13cac7dddc81d
Roger Taracha <rojtaracha@gmail.com>
2018-06-23 22:34:59 +0300

    [FEATURE] Working with the @Insert annotation:
    [+] @Insert annotation - used to inform the room compiler that you want to have an implementation that inserts all parameters to the DB, as a single transaction.

--- droid-sqlite/app/src/main/java/com/thedancercodes/recipe/app/db/RecipeDao.java
+++ droid-sqlite/app/src/main/java/com/thedancercodes/recipe/app/db/RecipeDao.java
@@ -1,7 +1,14 @@
 package com.thedancercodes.recipe.app.db;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+
+import com.thedancercodes.recipe.app.models.Recipe;
 
 @Dao
 public interface RecipeDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    long createRecipe(Recipe recipe); // This method returns the recipe Id
 }
--- droid-sqlite/app/src/main/java/com/thedancercodes/recipe/app/db/RecipeStepDao.java
+++ droid-sqlite/app/src/main/java/com/thedancercodes/recipe/app/db/RecipeStepDao.java
@@ -1,7 +1,16 @@
 package com.thedancercodes.recipe.app.db;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+
+import com.thedancercodes.recipe.app.models.RecipeStep;
+
+import java.util.List;
 
 @Dao
 public interface RecipeStepDao {
+
+    // using void as we aren't interested in the resulting IDs of the inserted recipe step objects.
+    @Insert
+    void insertAll(List<RecipeStep> steps);
 }
